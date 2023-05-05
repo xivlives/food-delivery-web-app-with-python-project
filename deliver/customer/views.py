e98eb67ba46239dc8a2c0d94d0bfae92a3b98ddf
favour orji <favour@learnflo.co>
2023-05-05 22:29:23 +0100

    first commit

---
+++ deliver/customer/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views import View
+
+class Index(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'customer/index.html')
+    
+
+class About(view):
+    def get(self, request, *args, **kwargs);
+        return render(request, 'customer/about.html')
+
