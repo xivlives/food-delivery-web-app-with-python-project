24b55645821cc954b0fc92cf379f86e04ed9a3ff
favour orji <favour@learnflo.co>
2023-05-20 13:52:49 +0100

    modal expansion

--- deliver/customer/models.py
+++ deliver/customer/models.py
@@ -22,6 +22,13 @@ class OrderModel(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     items = models.ManyToManyField('menuItem', related_name='order', blank=True)
+    name = models.CharField(max_length=50, blank=True)
+    email = models.EmailField(max_length=50, blank=True)
+    street = models.CharField(max_length=50, blank=True)
+    city = models.CharField(max_length=50, blank=True)
+    state = models.CharField(max_length=15, blank=True)
+    zip_code = models.IntegerField(blank=True, null=True)
+    
 
     def __str__(self):
         return f'Order: {self.created_on.strftime("%b %I: %M %p")}'
